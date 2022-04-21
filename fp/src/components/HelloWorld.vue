44c08aaabab546c2f8c71bcc2a165bb862871ae9
Tyagachev Anton <570uz@mail.ru>
2022-04-21 13:43:26 +0300

    ДЗ № 2

--- fp/src/components/HelloWorld.vue
+++ fp/src/components/HelloWorld.vue
@@ -4,65 +4,36 @@
 
     <hr>
 
-    <input v-model.number="operand1" ref="input1" />
-    <input v-model.number="operand2" ref="input2" />
+    <input type="number" v-model.number.lazy="operand1" ref="input1" />
+    <input type="number"  v-model.number.lazy="operand2" ref="input2" />
     <p v-if="result == Infinity">На ноль делить нельзя</p>
     <p v-else>= {{ result }}</p>
     <br>
-    <button @click="sum()">+</button>
-    <button @click="substract()">-</button>
-    <button @click="multiply()">*</button>
-    <button @click="divide()">/</button>
+    <button class ="btn__calc" @click="sum()">+</button>
+    <button class ="btn__calc" @click="substract()">-</button>
+    <button class ="btn__calc" @click="multiply()">*</button>
+    <button class ="btn__calc" @click="degree()">**</button>
+    <button class ="btn__calc" @click="divide()">/</button>
+    <button class ="btn__calc" @click="remainder()">%</button>
+    <button class ="btn__calc" @click="removebutton()">R</button>
 
     <hr>
 
     <div class="test">
           <input type="checkbox" id="checkbox" v-model="checked" />Отобразить клавиатуру
           <div v-if="checked == true">
-          <button @click="input(num)" v-for="num in numbers" :key="num">{{ num }}</button>
+            <div class="btn__wrp">
+          <button class="btn" @click="input(num)" v-for="num in numbers" :key="num">{{ num }}</button>
+          </div>
         </div>
     </div>
     
     <input @click="foc1()" type="radio" name="test" value="operand1" v-model="picked" />Операнд 1
     <input @click="foc2()" type="radio" name="test" value="operand2" v-model="picked" />Операнд 2
-    <!--<div v-for="pic in picked" :key="pic">{{pic}}</div>-->
-
-    
-  
-
-
-    <!--<p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-mocha" target="_blank" rel="noopener">unit-mocha</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-e2e-cypress" target="_blank" rel="noopener">e2e-cypress</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>-->
   </div>
 </template>
+
+
 
 <script>
 export default {
@@ -74,22 +45,35 @@ export default {
   data() {
     return {
       result: '',
-      operand1: 0,
-      operand2: 0,
+      operand1: '',
+      operand2: '',
       checked: true,
-      numbers: [1, 2, 4, 5, 6, 7, 8, 9, 0],
-      operands: ["+", "-", "/", "*"],
+      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+      operands: ["+", "-", "/", "*","**", "%","<--"],
       picked:[],
     };
   },
 
   methods: {
     input(test) {
-      if(this.picked==this.operand1){
-        this.operand1+=test
+      if(this.picked === "operand1"){
+        this.operand1 += parseInt(test)
+      }else{
+        this.operand2 += parseInt(test)
       }
-      if(this.picked==this.operand2){
-        this.operand2+=test
+    },
+
+    removebutton() {
+      if(this.operand1.length>1) {
+        this.operand1 = this.operand1.slice(0,this.operand1.length-1)
+      }else{
+        this.operand1=''
+      }
+
+      if(this.operand2.length>1) {
+        this.operand2 = this.operand2.slice(0,this.operand2.length-1)
+      }else{
+        this.operand2=''
       }
     },
 
@@ -105,8 +89,17 @@ export default {
         case "*":
           this.multiply();
           break;
+        case "**":
+          this.degree();
+          break;
         case "/":
           this.divide();
+          break;
+        case "%":
+          this.remainder();
+          break;
+        case "<--":
+          this.removebutton();
           break;
       }
     },
@@ -120,7 +113,7 @@ export default {
     },
 
     sum(){
-      this.result = this.operand1 + this.operand2
+      this.result = Number(this.operand1) + Number(this.operand2)
     },
 
     substract() {
@@ -134,6 +127,14 @@ export default {
     multiply() {
       this.result = this.operand1 * this.operand2
     },
+
+    degree() {
+      this.result = this.operand1 ** this.operand2
+    },
+
+    remainder(){
+      this.result = this.operand1 % this.operand2
+  },
   },
 };
 
@@ -155,4 +156,23 @@ li {
 a {
   color: #42b983;
 }
+
+
+.btn__wrp {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  width: 100px;
+  margin: 0 auto;
+}
+.btn {
+  width: 50px;
+  height: 50px;
+}
+.btn__calc {
+  width: 30px;
+  height: 30px;
+  margin: 3px;
+}
+
 </style>
